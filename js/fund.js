2bfab5f8d6cb5b7174ccf761725dc99ef092ae4f
Md Hashinur Rashid Sohan <m0shn@outlook.com>
2022-02-18 14:45:21 +0600

    all calculation done

--- js/fund.js
+++ js/fund.js
@@ -1,16 +1,17 @@
 function calculationCalling(call) {
-    const ipnutIncome = document.getElementById(call);
-    const ipnutIncomeText = ipnutIncome.value;
-    const inceme = parseFloat(ipnutIncomeText)
-    console.log(inceme);
-    return inceme;
+    const ipnut = document.getElementById(call);
+    const ipnutText = ipnut.value;
+    const output = parseFloat(ipnutText)
+    return output;
 }
 
 document.getElementById('calculation-button').addEventListener('click', function () {
+    //    calling function
     const incomeInput = calculationCalling('income-input');
     const foodInput = calculationCalling('food-input');
     const rentInput = calculationCalling('rent-input');
     const clothInput = calculationCalling('cloth-input');
+    // calculation total expences
     const totalExpences = foodInput + rentInput + clothInput;
     console.log(totalExpences);
     const getExpences = document.getElementById('total-expenses');
@@ -23,4 +24,44 @@ document.getElementById('calculation-button').addEventListener('click', function
     const getBalanceText = getBalance.innerText;
     const nowBalance = parseFloat(getBalanceText);
     getBalance.innerText = balance;
+})
+
+
+// svaing amount calculation
+
+document.getElementById('save-button').addEventListener('click', function () {
+    // gating saving input
+    const getSaveInput = document.getElementById('save-input');
+    const saveInputText = getSaveInput.value;
+    const saveInput = parseFloat(saveInputText);
+    // function calling
+    const incomeInput = calculationCalling('income-input');
+    // save amount calculation
+    const saveAmount = (incomeInput * (saveInput / 100));
+
+
+
+    // saving amount updating
+    const getSavingAmount = document.getElementById('save-amount');
+    const savingAmountText = getSavingAmount.innerText;
+    const savingAmount = parseFloat(savingAmountText);
+    // updating saving amount
+    getSavingAmount.innerText = saveAmount;
+
+
+    //    calling function
+    // const incomeInput = calculationCalling('income-input');
+    const foodInput = calculationCalling('food-input');
+    const rentInput = calculationCalling('rent-input');
+    const clothInput = calculationCalling('cloth-input');
+    // calculation total expences
+    const totalExpences = foodInput + rentInput + clothInput;
+    // remaining Balance calcilation
+    const remainingBalance = incomeInput - saveAmount - totalExpences;
+    const getRemainingBalance = document.getElementById('remaining-balance');
+    const remainingBalanceText = getRemainingBalance.innerText;
+    const updatedRemainingBalance = parseFloat(remainingBalanceText);
+    // updating remaining balance amount
+    getRemainingBalance.innerText = remainingBalance;
+
 })
